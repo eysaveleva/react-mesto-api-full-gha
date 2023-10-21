5fc69b76bd82981f56b1064109753ebc1538d216
eysaveleva <110624031+eysaveleva@users.noreply.github.com>
2023-10-21 16:49:23 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
-const { KEY } = require('../config/config');
+const { NODE_ENV, JWT_SECRET, KEY } = require('../config/config');
 
 module.exports = (req, res, next) => {
   // достаём авторизационный заголовок
@@ -13,7 +13,8 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, KEY);
+    // payload = jwt.verify(token, KEY);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : KEY);
   } catch (err) {
     // отправим ошибку, если не получилось
     return next(new UnauthorizedError('Необходима авторизация'));
